3e446d91f37cd7a391fd895d0a9b7dfd28b9666e
Motawala <pkaran1100@gmail.com>
2024-02-08 14:28:37 -0500

    Hide and show function still in progress

---
+++ Directed Graph/createNode.js
@@ -0,0 +1,367 @@
+
+var PORT_WIDTH = 90;
+const PORT_HEIGHT = 20;
+const PORT_GAP = 20;
+
+
+
+function makeLink(from,to) {
+    const link = new joint.shapes.standard.Link({
+       source: { id: from.id},
+       target: { id: to.id },
+       attr:{
+        '.connection': { stroke: 'red', 'stroke-dasharray': '5,5' }
+       }
+     });
+     link.set('hidden', true);
+      return link
+ }
+
+
+function createStage(id, name){
+    const node = new joint.shapes.standard.Rectangle({
+        id: id,
+        position: {
+          x: 250,
+          y: 500
+        },
+        size: {
+          width: '450',
+          height: 45
+        },
+        attrs: {
+          label: {
+            fontWeight: "bold",
+            fontSize: 10,
+            fontFamily: "sans-serif",
+            fill: "#ffffff",
+            stroke: "#333333",
+            strokeWidth: 5,
+            paintOrder: "stroke",
+            text: name,
+           },
+           body: {
+            strokeWidth: 2,
+            fill: "grey",
+            cursor: "grab"
+          },
+        },
+        ports: {
+          groups: {
+            rdaf: {
+              markup: [
+                {
+                  tagName: "rect",
+                  selector: "portBody"
+                },
+                {
+                  tagName: "text",
+                  selector: "portLabel"
+                }
+              ],
+              attrs: {
+                portBody: {
+                  x: 0,
+                  y: -PORT_HEIGHT / 2,
+                  width: "calc(w)",
+                  height: "calc(h)",
+                  fill: "yellow",
+                  stroke: "#333333",
+                  strokeWidth: 2,
+                  //magnet: "active", //this allows the user to drag an connect ports
+                  cursor: "grab",
+                },
+                portLabel: {
+                  x: "calc(0.5*w)",
+                  textAnchor: "middle",
+                  textVerticalAnchor: "middle",
+                  pointerEvents: "none",
+                  fontWeight: "bold",
+                  fontSize: 12,
+                  fontFamily: "sans-serif"
+                },
+              },
+              size: { width: PORT_WIDTH, height: PORT_HEIGHT },
+              position: "absolute"
+            },
+          },
+          items: []
+        }
+      });
+      //node.set('hidden', true);
+      return node
+}
+
+
+
+function createTopics(id, name){
+    const node = new joint.shapes.standard.Rectangle({
+        id: id,
+        position: {
+          x: 250,
+          y: 500
+        },
+        size: {
+          width: '250',
+          height: 45
+        },
+        attrs: {
+          label: {
+            fontWeight: "bold",
+            fontSize: 10,
+            fontFamily: "sans-serif",
+            fill: "#ffffff",
+            stroke: "#333333",
+            strokeWidth: 5,
+            paintOrder: "stroke",
+            text: name,
+           },
+           body: {
+            strokeWidth: 2,
+            fill: "#4d80b3",
+            cursor: "grab"
+          },
+        },
+        ports: {
+          groups: {
+            rdaf: {
+              markup: [
+                {
+                  tagName: "rect",
+                  selector: "portBody"
+                },
+                {
+                  tagName: "text",
+                  selector: "portLabel"
+                }
+              ],
+              attrs: {
+                portBody: {
+                  x: 0,
+                  y: -PORT_HEIGHT / 2,
+                  width: "85",
+                  height: "calc(h)",
+                  fill: "white",
+                  stroke: "#333333",
+                  strokeWidth: 2,
+                  //magnet: "active", //this allows the user to drag an connect ports
+                  cursor: "grab",
+                },
+                portLabel: {
+                  x: "calc(0.5*w)",
+                  textAnchor: "middle",
+                  textVerticalAnchor: "middle",
+                  pointerEvents: "none",
+                  fontWeight: "bold",
+                  fontSize: 12,
+                  fontFamily: "sans-serif"
+                },
+              },
+              size: { width: PORT_WIDTH, height: PORT_HEIGHT },
+              position: "absolute"
+            },
+          },
+          items: []
+        }
+      });
+      node.set('hidden', true);
+      return node
+ }
+
+ function createRoles(id, name){
+
+    return new joint.shapes.standard.Rectangle({
+        id: id,
+        position: { x:100, y: 50 },
+        size: { width: 100, height: 40 },
+        attrs: {
+            rect: { fill: 'lightblue', stroke: 'blue', 'stroke-width': 2 },
+            text: { text: 'Rect', fill: 'black' },
+        },
+        label: {
+            fontWeight: "bold",
+            fontSize: 10,
+            fontFamily: "sans-serif",
+            fill: "#ffffff",
+            stroke: "#333333",
+            strokeWidth: 5,
+            paintOrder: "stroke",
+            text: name,
+            cursor: "text"
+        },
+        body: {
+            strokeWidth: 2,
+            fill: "#336699",
+            cursor: "grab"
+        }
+    });
+ }
+
+ function considerations(id, name){
+
+    return new joint.shapes.standard.Rectangle({
+        id: id,
+        position: { x:100, y: 50 },
+        size: { width: 100, height: 40 },
+        attrs: {
+            rect: { fill: 'lightblue', stroke: 'blue', 'stroke-width': 2 },
+            text: { text: 'Rect', fill: 'black' },
+        },
+        label: {
+            fontWeight: "bold",
+            fontSize: 10,
+            fontFamily: "sans-serif",
+            fill: "#ffffff",
+            stroke: "#333333",
+            strokeWidth: 5,
+            paintOrder: "stroke",
+            text: name,
+            cursor: "text"
+        },
+        body: {
+            strokeWidth: 2,
+            fill: "#336699",
+            cursor: "grab"
+        }
+    });
+ }
+
+
+ function createMethods(id, name){
+
+    return new joint.shapes.standard.Rectangle({
+        id: id,
+        position: { x:100, y: 50 },
+        size: { width: 100, height: 40 },
+        attrs: {
+            rect: { fill: 'lightblue', stroke: 'blue', 'stroke-width': 2 },
+            text: { text: 'Rect', fill: 'black' },
+        },
+        label: {
+            fontWeight: "bold",
+            fontSize: 10,
+            fontFamily: "sans-serif",
+            fill: "#ffffff",
+            stroke: "#333333",
+            strokeWidth: 5,
+            paintOrder: "stroke",
+            text: name,
+            cursor: "text"
+        },
+        body: {
+            strokeWidth: 2,
+            fill: "#336699",
+            cursor: "grab"
+        }
+    });
+ }
+
+ function createOutcomes(id, name){
+    const textWidth = name.length * 6; // Approximate width based on font size and average character width
+    const width = Math.max(textWidth, 100); // Ensure a minimum width to accommodate shorter text
+    const node =  new joint.shapes.standard.Rectangle({
+        id: id,
+        position: {
+          x: 250,
+          y: 500
+        },
+        size: {
+          width: width,
+          height: 45
+        },
+        attrs: {
+          label: {
+            fontWeight: "bold",
+            fontSize: 10,
+            fontFamily: "sans-serif",
+            fill: "#ffffff",
+            stroke: "#333333",
+            strokeWidth: 5,
+            paintOrder: "stroke",
+            text: name,
+           },
+           body: {
+            strokeWidth: 2,
+            fill: "#4d80b3",
+            cursor: "grab"
+          },
+        },
+        ports: {
+          groups: {
+            rdaf: {
+              markup: [
+                {
+                  tagName: "rect",
+                  selector: "portBody"
+                },
+                {
+                  tagName: "text",
+                  selector: "portLabel"
+                }
+              ],
+              attrs: {
+                portBody: {
+                  x: 0,
+                  y: -PORT_HEIGHT / 2,
+                  width: "85",
+                  height: "calc(h)",
+                  fill: "white",
+                  stroke: "#333333",
+                  strokeWidth: 2,
+                  //magnet: "active", //this allows the user to drag an connect ports
+                  cursor: "grab",
+                },
+                portLabel: {
+                  x: "calc(0.5*w)",
+                  textAnchor: "middle",
+                  textVerticalAnchor: "middle",
+                  pointerEvents: "none",
+                  fontWeight: "bold",
+                  fontSize: 12,
+                  fontFamily: "sans-serif"
+                },
+              },
+              size: { width: PORT_WIDTH, height: PORT_HEIGHT },
+              position: "absolute"
+            },
+          },
+          items: []
+        }
+      });
+      node.set('hidden', true);
+      return node
+}
+
+
+
+function setPorts(el, ports) {
+    let width = 0;
+    const rdafPorts = ports.map((port, index) => {
+      const x = index * (PORT_WIDTH + PORT_GAP);
+      width = x + PORT_WIDTH;
+      return {
+        id: `${port}`,
+        group: "rdaf",
+        attrs: {
+          portLabel: {
+            text: `${port}`,
+            
+          }
+        },
+        args: {
+          x: "90%",
+          y: "50%"
+        },
+      };
+    });
+    if (rdafPorts.length > 0) {
+      width += PORT_GAP;
+    }
+  
+    width += 2 * PORT_WIDTH;
+  
+    el.prop(["ports", "items"], [...rdafPorts], {
+      rewrite: true
+    });
+  
+  }
--- Directed Graph/direct.js
+++ Directed Graph/direct.js
@@ -7,7 +7,7 @@ const paper = new dia.Paper({
     el: document.getElementById('graph-container'),
     model: graph,
     width: 10000,
-    height: 1000,
+    height: 10000,
     gridSize: 10,
     drawGrid: true,
     background: {
@@ -20,165 +20,16 @@ const paper = new dia.Paper({
         return !view.model.get('hidden');
     }
 });
-
-// Define nodes and links
-var rect = new joint.shapes.standard.Rectangle({
-    position: { x: 100, y: 50 },
-    size: { width: 100, height: 40 },
-    attrs: {
-        rect: { fill: 'lightblue', stroke: 'blue', 'stroke-width': 2 },
-        text: { text: 'Rect', fill: 'black' }
-    },
-            
-});
-
-        
-        
-var rect2 = new joint.shapes.standard.Rectangle({
-    position: { x: 100, y: 50 },
-    size: { width: 100, height: 40 },
-    attrs: {
-        rect: { fill: 'lightblue', stroke: 'blue', 'stroke-width': 2 },
-        text: { text: 'Rect', fill: 'black' }
-    }
-});
-
-var rect3 = new joint.shapes.standard.Rectangle({
-    position: { x: 100, y: 50 },
-    size: { width: 100, height: 40 },
-    attrs: {
-        rect: { fill: 'lightblue', stroke: 'blue', 'stroke-width': 2 },
-        text: { text: 'Rect', fill: 'black' }
-    }
-});
-       
-
-var rectButton = new joint.shapes.standard.Rectangle({
-    position: { x: 100, y: 50 },
-    size: { width: 100, height: 15 },
-    attrs: {
-        rect: { fill: 'lightblue', stroke: 'blue', 'stroke-width': 2 },
-        text: { text: 'Rect', fill: 'black' }
-    }
-});
-
-
-var circle = new joint.shapes.standard.Circle({
-    position: { x: 300, y: 50 },
-    size: { width: 100, height: 100 },
-    attrs: {
-        circle: { fill: 'lightgreen', stroke: 'green', 'stroke-width': 2 },
-        text: { text: 'Circle', fill: 'black' }
-    }
-});
-
-var circle2 = new joint.shapes.standard.Circle({
-    position: { x: 300, y: 50 },
-    size: { width: 100, height: 100 },
-    attrs: {
-        circle: { fill: 'lightgreen', stroke: 'green', 'stroke-width': 2 },
-        text: { text: 'Circle', fill: 'black' }
-    }
-});
-
-var link = new joint.shapes.standard.Link({
-    source: { id: rect.id },
-    target: { id: circle.id },
-    attrs: {
-        '.marker-target': { d: 'M 10 0 L 0 5 L 10 10 z' },
-        '.connection': { stroke: 'black', 'stroke-width': 2 }
-    }
-});
-        
-var link2 = new joint.shapes.standard.Link({
-    source: { id: circle.id },
-    target: { id: circle2.id },
-    attrs: {
-        '.marker-target': { d: 'M 10 0 L 0 5 L 10 10 z' },
-        '.connection': { stroke: 'black', 'stroke-width': 2 }
-    }
-});
-
-var link3 = new joint.shapes.standard.Link({
-    source: { id: rect.id },
-    target: { id: rect2.id},
-    attrs: {
-        '.marker-target': { d: 'M 10 0 L 0 5 L 10 10 z' },
-        '.connection': { stroke: 'black', 'stroke-width': 2 }
-    }
-});
-
-
-var link4 = new joint.shapes.standard.Link({
-    source: { id: rect2.id },
-    target: { id: circle.id },
-    attrs: {
-    '.marker-target': { d: 'M 10 0 L 0 5 L 10 10 z' },
-                '.connection': { stroke: 'black', 'stroke-width': 2 }
-    }
-});
-
-var link5 = new joint.shapes.standard.Link({
-    source: { id: rect2.id },
-    target: { id: rect3.id },
-    attrs: {
-    '.marker-target': { d: 'M 10 0 L 0 5 L 10 10 z' },
-                '.connection': { stroke: 'black', 'stroke-width': 2 }
-    }
-});
-
         
 paper.on('element:pointerclick', function(view, evt) {
     evt.stopPropagation();
     toggleBranch(view.model);
 })
         
-        
-
-function toggleBranch(child) {
-    if(child.isElement()){
-        //retrives the value of the collapsed attribute from the root model.
-        var shouldHide = child.get('collapsed');
-        child.set('collapsed', !shouldHide);
-        // Handle links connected to the child, inbound set to false so that the root element does not collapse
-        const links = graph.getConnectedLinks(child, { inbound: false, outbound: true });
-        //Array of all the links that goes out of the root
-        links.forEach(function(link) {
-            // Set the 'hidden' attribute for the link
-            link.set('hidden', shouldHide);
-        });
-        //Sets the element to collapse (true or false)
-        //Successor Cells collects all the elements that are linked to the current(active) element.
-        var successrorCells = graph.getSubgraph([
-            ...graph.getSuccessors(child),
-        ])
-        successrorCells.forEach(function(successor) {
-            successor.set('hidden', shouldHide);
-            successor.set('collapsed', false);
-        });
-    }
-}
-var Element1 = []
-Element1.push(rect)
-Element1.push(circle)
-Element1.push(link)
-Element1.push(circle2)
-Element1.push(link2)
-Element1.push(rect2)
-Element1.push(link3)
-Element1.push(link4)
-Element1.push(rect3)
-Element1.push(link5)
-graph.addCells([Element1])
-
-
-
-
 
 function buildTheGraph(){
-    var Elements = []
-    var Links = []
-    fetch('sample.jsonld')
+  var Elements = []
+    fetch('graph.jsonld')
    .then(response => response.json())
    // specifying a frame allows you to set the shape of the graph you want to navigate
    .then(data => jsonld.frame(data,
@@ -210,43 +61,43 @@ function buildTheGraph(){
               "@type": "@id"
             },
         },
-        
-       
-    
+           
    }))
    .then(frame => {
       // example of pulling a single Outcome and linked Activity from the input data file
       // in reality we want to navigate the entire graph
       const frameArray = frame['@graph']
-      frameArray.forEach(topic =>{
-        stage = topic;
-        console.log(stage['additionalType'])
-        if(stage['additionalType'] == "RdAF Stage"){
-            stageElement = createStage(stage['@id'], stage['name'])
-            Elements.push(stageElement)
-            //graph.addCells([stageElement]);
-            
-        }
-        if(stage['additionalType'] == "RdAF Topic"){
-            stageElement2 = makeStage(stage['@id'], stage['name'])
-            const link1 = makeLink(stageElement,stageElement2)
-            Elements.push(stageElement2)
-            Elements.push(link1)
-            //graph.addCells([stageElement2, link1]);
-        }if(stage['additionalType'] == "RdAF Subtopic"){
-            stageElement3 = createStage(stage['@id'], stage['name'])
-            const link1 = makeLink(stageElement2,stageElement3)
-            Elements.push(stageElement3)
-            Elements.push(link1)
-        }
-
-        
+      frameArray.forEach(node =>{
+            if(node['additionalType'] == "RdAF Stage"){
+              const element1 = createStage(node['@id'], node['name'])
+              setPorts(element1, ['Topics']);
+              Elements.push(element1)
+              topic = node['sunyrdaf:includes']
+              if(Array.isArray(topic)){
+                topic.forEach(topics =>{
+                  if(topics){
+                    const element2 = createTopics(topics['@id'], topics['name'])
+                    const linkStageToTopics = makeLink(element1, element2)
+                    //element2.set('hidden', true)
+                    //linkStageToTopics.set('hidden', true)
+                    linkStageToTopics.labels([{
+                      attrs:{
+                        text:{
+                          text:"Topics"
+                        }
+                      }
+                    }])
+                    checkOutcomes(topics, Elements, element2)
+                    Elements.push([element2, linkStageToTopics])
+                    
+                  }
+                })
+              }
+              
+            }
       });
-
-    
-      graph.addCells([Elements])
+      graph.addCells(Elements)
       //graph.addCells(Elements.concat(Links))
-      //console.log(Elements.concat(Links))
       joint.layout.DirectedGraph.layout(graph, {
         setLinkVertices: false, // Optional: Prevent the plugin from setting link vertices
         nodeSep: 50,
@@ -255,85 +106,41 @@ function buildTheGraph(){
         ranker: 'network-simplex'
     });
    })
-
-   
-    
 }
 
 
-function makeLink(from,to) {
-    return new joint.shapes.standard.Link({
-       source: { id: from.id},
-       target: { id: to.id },
-       attr:{
-        '.connection': { stroke: 'red', 'stroke-dasharray': '5,5' }
-       }
-     });
- }
-
-
- function makeStage(id, name){
-
-    return new joint.shapes.standard.Rectangle({
-        id: id,
-        position: { x:100, y: 50 },
-        size: { width: 100, height: 40 },
-        attrs: {
-            rect: { fill: 'lightblue', stroke: 'blue', 'stroke-width': 2 },
-            text: { text: 'Rect', fill: 'black' }
-        },
-        label: {
-            fontWeight: "bold",
-            fontSize: 10,
-            fontFamily: "sans-serif",
-            fill: "#ffffff",
-            stroke: "#333333",
-            strokeWidth: 5,
-            paintOrder: "stroke",
-            text: name,
-            cursor: "text"
-        }
-    });
- }
-
-
-function createStage(id, name){
-    var node = new joint.shapes.standard.Rectangle({
-        id: id,
-        position: {
-            x: 0,
-            y: 250
-        },
-        size: {
-            width: 350,
-            height: 45
-        },
-        attrs: {
-            root: {
-            magnet: false
-        },
-        body: {
-            strokeWidth: 2,
-            fill: "#336699",
-            cursor: "grab"
-        },
-        label: {
-            fontWeight: "bold",
-            fontSize: 10,
-            fontFamily: "sans-serif",
-            fill: "#ffffff",
-            stroke: "#333333",
-            strokeWidth: 5,
-            paintOrder: "stroke",
-            text: name,
-            cursor: "text"
-        }
-        },
-    });
-    return node;
+function checkOutcomes(topic, arr, parentNode){
+  for (const key in topic){
+      if(key.startsWith('sunyrdaf')){
+          if(key == "sunyrdaf:generates"){
+              if(Array.isArray(topic[key])){
+                  topic[key].forEach(outcomes =>{
+                      const out = createOutcomes(outcomes['@id'], outcomes['name'])
+                      const linkTopicToOutcome = makeLink(parentNode, out)
+                      
+                      //out.set('hidden', true)
+                      //linkTopicToOutcome.set('hidden',true)
+                      linkTopicToOutcome.labels([{
+                        attrs:{
+                          text:{
+                            text:"Outcomes"
+                          }
+                        }
+                      }])
+                      arr.push([out])
+                      arr.push([linkTopicToOutcome])
+                  })
+              }
+          }
+      }
+  
+  }
+  
 }
+
 
 buildTheGraph();
+
 
 // Add nodes and link to the graph
 //graph.addCells([rect, circle, link, circle2, link2, rect2, link3, link4, rect3, link5]);
@@ -345,5 +152,9 @@ joint.layout.DirectedGraph.layout(graph, {
     rankDir: "LR",
     ranker: 'network-simplex'
 });
+
+
+
+
 
 
