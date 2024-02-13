20c7dafd639a7432229ad491c591ec093d0c941f
Bridget Almas <balmas@gmail.com>
2024-02-13 07:49:24 -0500

    experimenting with node positioning

--- Directed Graph/events.js
+++ Directed Graph/events.js
@@ -8,6 +8,9 @@ function toggleBranch(child) {
 }
 
 function openBranch(child, shouldHide){
+    // TODO: openBranch should only open the next level of the tree, not the
+    // full tree - this might also help with the initial layout
+
     //Finds the outgoing links to the element the user is interacting with
     const findTarget = graph.getConnectedLinks(child, {outbound:true})
     //Using each link that is connected find the target Elements and play with them
@@ -18,6 +21,7 @@ function openBranch(child, shouldHide){
         element.set('collapsed', false)
         //Make the links visible
         targetLink.set('hidden', shouldHide)
+	
         if(!element.get('collapsed')){
             console.log(element.get('collapsed'))
             const subElementsLinks = graph.getConnectedLinks(element, {outbound:true})
@@ -31,6 +35,11 @@ function openBranch(child, shouldHide){
             })
             
         }
+	// I don't think I'm calling this correctly, or in the right
+	// place but see https://resources.jointjs.com/docs/jointjs/v3.7/joint.html#dia.LinkView.prototype.requestConnectionUpdate
+	// in theory it should help with recalculating the routes after
+	// things move
+	paper.findViewByModel(targetLink).requestConnectionUpdate();
         
     })
 
